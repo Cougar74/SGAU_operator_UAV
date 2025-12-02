3b3c79ae686c445c1b8d5b08e55daced021fe3d0
Cougar <edd_eme@mail.ru>
2025-12-02 21:19:32 +0300

    Update practice #1 (add functions), add practice #2 (OOP)

---
+++ Практика/Практика - Основы Python/describe_pet.py
@@ -0,0 +1,7 @@
+def describe_pet_name(name_pet, animal_type = 'собака'):
+    print(f'У меня "{animal_type}" и его зовут {name_pet}.')
+    
+    return name_pet.lower()
+
+if __name__ == '__main__':
+    pass
